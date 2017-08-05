a675164887d2713d61760e1a99dcda853e6993b6
Yunki <shinki95@gmail.com>
2017-08-06 01:47:10 +0900

    tag에 get인자로 next,prev 구성

--- shop/views.py
+++ shop/views.py
@@ -9,13 +9,17 @@ from .utils.collab_filtering import *
 
 def post_tag(request, tag):
     qs_tag = Post.objects.filter(tag_set__name__iexact=tag)
+
+    
     return render(request, 'shop/post_tag.html',{
+        'tag': tag,
         'shop_list_tag': qs_tag
     })
 
 
 def post_list(request):
     qs = Post.objects.all()
+
     return render(request, 'shop/post_list.html', {
         'shop_list':qs,
         'recommendation': user_recommendations(str(request.user))
@@ -24,12 +28,13 @@ def post_list(request):
 def post_detail(request, id):
     tag = request.GET.get('tag','')
     post = get_object_or_404(Post, id=id)
+
     next_post_list = Post.objects.filter(id__gt=post.id).order_by('id')
     prev_post_list = Post.objects.filter(id__lt=post.id).order_by('-id')
 
     if tag:
         next_post_list = next_post_list.filter(tag_set__name__iexact=tag)
-        prev_post_list = prev_post_list.fitler(tag_set__name__iexact=tag)
+        prev_post_list = prev_post_list.filter(tag_set__name__iexact=tag)
 
     return render(request, 'shop/post_detail.html', {
         'post': post,
